ad20e50e7ff1f08f4eaeabffac0d9fbff69a5540
MenghongLi <lp_meng23@live.cn>
2023-04-04 11:26:30 +1000

    update nvim

--- nvim/lua/menghong/remap.lua
+++ nvim/lua/menghong/remap.lua
@@ -26,7 +26,6 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>y", "\"+y")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<space>", "/")
